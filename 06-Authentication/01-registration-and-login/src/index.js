930964b2d9ec864660bfb542e16d0478f4c0fa08
Ande Praveen <praveenande2023@gmail.com>
2024-07-03 12:28:22 +0530

    updated authentication code

--- 06-Authentication/01-registration-and-login/src/index.js
+++ 06-Authentication/01-registration-and-login/src/index.js
@@ -49,12 +49,42 @@ const initializeDBAndServer = async () => {
 initializeDBAndServer();
 
 /* -----> Handling HTTP Request <----- */
-// 00 Test
+// 00 API -> Test
 app.get('/', (request, response) => {
-  response.send('Hello World!') 
+    console.log("Hello World")
+  response.send('Hello World') 
 })
 
-// 01 Registration -> Add User
+
+/* -----> Middlewares <----- */
+
+// authenticateToken Middleware
+const authenticateToken = (request, response, next) => {
+    console.log("authenticateToken Middleware");
+    let jwtToken;
+    const authHeader = request.headers["authorization"];
+    if (authHeader !== undefined) {
+        jwtToken = authHeader.split(" ")[1];
+    }
+    if (jwtToken === undefined) {
+        response.status(401).send("Invalid Access Token");
+    } else {
+        const secretKey = "MY_SECRET_TOKEN";  // It takes from .env
+        jwt.verify(jwtToken, secretKey, async (error, payload) => {
+            if (error) {
+                response.status(401).send("Invalid Access Token");
+            } else {
+                next();
+            }
+        });
+    }
+
+};
+
+
+/* -----> Routes <----- */
+
+// 01 API -> Registration -> Add User
 app.post('/users', async (request, response) => {
     console.log("Add User");
     // console.log(request.body);
@@ -97,7 +127,7 @@ app.post('/users', async (request, response) => {
     }
 })
 
-// 02 Login
+//  02 API -> Login
 app.post('/users/login', async (request, response) => {
     console.log("Login");
     const userDetails = request.body;
@@ -124,10 +154,11 @@ app.post('/users/login', async (request, response) => {
         const dbUserPassword = dbUser.password;
         const isPasswordMatched = await bcrypt.compare(password, dbUserPassword);
         if (isPasswordMatched === true) {
+            
             const payload = {
                 email:email
             };
-            const secretKey = password;
+            const secretKey = "MY_SECRET_TOKEN";   // It takes from .env
             const options = {
                 expiresIn: '1h', // Token expires in 1 hour
             }
@@ -145,4 +176,35 @@ app.post('/users/login', async (request, response) => {
             response.send("Invalid Password");
         }
     }
-})
+});
+
+// 03 API -> JWT Token Checking
+app.get("/home", (request, response) => {
+    console.log("Home");
+    // console.log(request.headers);
+    let jwtToken;
+    const authHeader = request.headers["authorization"];
+    if (authHeader !== undefined) {
+        jwtToken = authHeader.split(" ")[1];
+    }
+
+    if (jwtToken === undefined) {
+        response.status(401).send("Invalid Access Token");
+    } else {
+        const secretKey = "MY_SECRET_TOKEN";  // It takes from .env
+        jwt.verify(jwtToken, secretKey, async (error, payload) => {
+            if (error) {
+                response.status(401).send("Invalid Access Token");
+            } else {
+                response.status(200).send("Successfully Accessed users API");
+            }
+        });
+    }
+});
+
+
+// 04 API -> JWT Token Checking -> middleware
+app.get("/about", authenticateToken, (request, response) => {
+    console.log("About");
+    response.status(200).send("Successfully Accessed users API");
+});
